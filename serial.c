bb735247251332159d575c449a7c6f7bdf73073f
shaurma228 <sirnikdziha@gmail.com>
2025-06-19 19:51:08 +0500

    Added loops

--- serial.c
+++ serial.c
@@ -2,18 +2,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+double START;
+double END;
+
 int main(int argc, char* argv[]) {
     const int N = atoi(argv[1]);
+    const int LOOPS = atoi(argv[2]);
     int data[N];
+    double totalTime = 0.0;
 
-    const double START = omp_get_wtime();
-    long long sum = 0;
+    for (int iteration = 0; iteration < LOOPS; ++iteration) {
+        START = omp_get_wtime();
+        long long sum = 0;
 
-    for (int i = 0; i < N; ++i) {
-        data[i] = 1;
-        sum += data[i];
+        for (int i = 0; i < N; ++i) {
+            data[i] = 1;
+            sum += data[i];
+        }
+
+        END = omp_get_wtime();
+        totalTime += (END - START);
     }
 
-    const double END = omp_get_wtime();
-    printf("Sum: %lld, Time taken: %f seconds\n", sum, END - START);
+    printf("Time taken: %f seconds\n", totalTime / (LOOPS*1.0));
 }
